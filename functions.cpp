66896eb619e8bec843d932a3863eae8cd601b914
jeffnerdy <110793443+jeffnerdy@users.noreply.github.com>
2023-03-28 17:22:06 +0000

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -1,30 +1,31 @@
-#include "functions.h"
+#include "b_functions.h"
 
 DWORD GetProcID(const char* procName)
 {
-    DWORD procID;
-    
-    const auto snapHandle = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    DWORD procID = 0;
+    HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
 
-    if (snapHandle != INVALID_HANDLE_VALUE)
+    if (hSnap == INVALID_HANDLE_VALUE)
     {
-        PROCESSENTRY32 procEntry;
-        procEntry.dwSize = sizeof(procEntry);
+        return procID;
+    }
+    
+    PROCESSENTRY32 procEntry;
+    procEntry.dwSize = sizeof(procEntry);
 
-        if (Process32First(snapHandle, &procEntry))
+    if (Process32First(hSnap, &procEntry))
+    {
+        do
         {
-            do
+            if (!strcmp(procEntry.szExeFile, procName))
             {
-                if (!strcmp(procEntry.szExeFile, procName))
-                {
-                    procID = procEntry.th32ProcessID;
-                    break;
-                }
-            } while (Process32Next(snapHandle, &procEntry));
+                procID = procEntry.th32ProcessID;
+                break;
+            }
+        } while (Process32Next(hSnap, &procEntry));
 
-        }
     }
 
-    CloseHandle(snapHandle);
+    CloseHandle(hSnap);
     return procID;
 }
