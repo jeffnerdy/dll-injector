2f280d4f7f6cfe89be1740ef3afaf6600cec2ae3
jeffnerdy <110793443+jeffnerdy@users.noreply.github.com>
2023-03-28 17:22:28 +0000

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,8 +1,11 @@
 #pragma once
 
-#define WIN32_LEAN_AND_MEAN
-
 #include <iostream>
+#include <vector>
+#include <cstring>
+#include <filesystem>
+
+#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <tlhelp32.h>
 
