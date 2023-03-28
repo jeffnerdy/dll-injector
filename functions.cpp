31505154dee0173c401cef5befb131824e1f983d
jeffnerdy <110793443+jeffnerdy@users.noreply.github.com>
2023-03-28 17:20:25 +0000

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -3,12 +3,9 @@
 DWORD GetProcID(const char* procName)
 {
     DWORD procID;
-
-    // getting a handle on the snapshot
+    
     const auto snapHandle = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
 
-    // iterating through the entries within the snapshot until
-    // an exact process is found then assigning its id to procID
     if (snapHandle != INVALID_HANDLE_VALUE)
     {
         PROCESSENTRY32 procEntry;
